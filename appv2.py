0e6e518e165ea5e0fae78b0dae5b7f32db7e00f9
paulkp <paul.k.pallaghy@gmail.com>
2020-07-02 12:03:25 +0000

    tryv1.py

--- appv2.py
+++ appv2.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template,request,redirect,url_for
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from dbtest import get_everything, get_index, get_data_index
+
+from tryv1 import data
 
 app = Flask(__name__)
 
@@ -10,7 +14,10 @@ def home():
     index = get_index()
     if request.method=='POST':
         back = request.form['article']
-        every = get_data_index(back)
+        if back !='':
+            every = get_data_index(back)
+        else:
+            every = get_everything()
         return render_template('home.html', index = index,data=zip(every.Author,every.Summary,every.Title,every.date_crawled,every.date_published,every.full_text,every.img,every.news_source))
     
     return render_template('home.html', index = index,data=zip(every.Author,every.Summary,every.Title,every.date_crawled,every.date_published,every.full_text,every.img,every.news_source))
@@ -23,5 +30,15 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html')
 
-if __name__=='__main__':
-        app.run(debug=False)
+
+def job():
+	data()
+
+
+scheduler = BackgroundScheduler()
+job = scheduler.add_job(job, 'interval', minutes=60)
+scheduler.start()
+
+
+#if __name__=='__main__':
+#        app.run(debug=False)
--- tryv1.py
+++ tryv1.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 
+from selenium.webdriver.chrome.options import Options
+
+
 import requests
 import pandas as pd
 from dbtest import send_data,search
@@ -14,174 +17,190 @@ websites = ['https://www.abc.net.au',
 'https://techcrunch.com',
 'https://www.theguardian.com']
 
+#websites = ['https://www.theguardian.com']
 
-asli = []
-
-
-
-for j in websites:
-    response = requests.get(j)
-        
-    soup = BeautifulSoup(response.text,'html.parser')
-    
-    url = soup.find_all('a')
-    
-    
-    for i in range(len(url)) :
-        try:
-            url[i] = url[i]['href']
-        except:
-            try:
-                url.remove(url[i])
-            except:
-                pass
-    
-    var=[]
-    for i in url:
-       if i not in var:
-           var.append(i)
-
-    
-    url = var
-   
-
-    try:
-        f  = open('urlparsed.txt','r')
-        already_parsed = f.read().split('\n')
-        f.close()
-    except:
-        f = open('urlparsed.txt','w')
-        for i in url:
-            try:
-                i =i['href']
-            except:
-                pass
-            f.write(str(i))
-            f.write('\n')
-        f.close()
-       
-    try:
-        for i in already_parsed:
-            try:
-                url.remove(i)
-            except:
-                pass
-        
-        for i in url:
-            already_parsed.append(i)
-        
-        f = open('urlparsed.txt','w')
-        for i in already_parsed:
-                try:
-                    i =i['href']
-                except:
-                    pass
-                f.write(str(i))
-                f.write('\n')
-        f.close()
-    except:
-        pass
-        
-    for i in url:    
-        try :
-            try:
-                i =i['href']
-            except:
-                pass
-            
-            
-            if 'https' not in i:
-                if 'http' not in i:
-                    i = j+i
-            
-            print('\n',i)
-                
-            response = requests.get(i)
-            details = newspaper(i)
-            count = len(details.article)
-            publish_date = details.date_publish
-            cr_date = details.date_download
-            description = details.description
-            summary = details.summary
-            category = details.category
-            if count > 1500:
-                if len(description) > 10 or len(summary) > 10:
-            
-                    print("Appended")
-                    asli.append(i)
-                    
-                else:
-                    pass
-            else:
-                pass
-        except:
-            pass
+def data():
+	asli = []
 
 
 
+	for j in websites:
+	    response = requests.get(j)
+	        
+	    soup = BeautifulSoup(response.text,'html.parser')
+	    
+	    url = soup.find_all('a')
+	    
+	    
+	    for i in range(len(url)) :
+	        try:
+	            url[i] = url[i]['href']
+	        except:
+	            try:
+	                url.remove(url[i])
+	            except:
+	                pass
+	    
+	    var=[]
+	    for i in url:
+	       if i not in var:
+	           var.append(i)
 
-headline=[]
-timestamp=[]
-AUTHORS =[]
-SUMMARY=[]
-date_crawled = []
-news_source = [] 
-full = []
-img_url = []
-for i in asli:
-    driver = webdriver.Chrome('/home/priyanshu/project 10/chromedriver')
-    driver.get(i)
-    details = newspaper(i)
-    headline.append(details.headline)
-    timestamp.append(details.date_publish)
-    author=''
-    for i in details.authors:
-        author = author + i
-        author =author + ', '
-        
-    
-    author= author[:-2]
-    AUTHORS.append(author)
-    
-    if len(details.summary) > 10:
-        SUMMARY.append(details.summary)
-    else:
-        SUMMARY.append(details.description)
-    
-    date_crawled.append(details.date_download)
-    news_source.append(details.source_domain)
-    
-    full.append(details.article)
-    
-    re = driver.find_elements_by_tag_name('img')
-    for i in re:
-        if'.jpg' in i.get_attribute('src'):
-            img_url.append(i.get_attribute('src'))
-            break;
-    
-    driver.close()
+	    
+	    url = var
+	   
 
-final = pd.DataFrame({'Title':headline,'Author':AUTHORS,'Summary':SUMMARY,
-                          'full_text':full,'date_published':timestamp, 'date_crawled':date_crawled,
-                          'news_source':news_source,'img':img_url})
-    
-for i in final.index:
-    try:
-        t = pd.DataFrame()
-        t =t.append(final.loc[i])
-        t.reset_index(drop=True, inplace=True)
-        try:
-            count = search(t.loc[0]['Title'],t.loc[0]['news_source'])
-            print(count)
-            if count < 25 :
-                test =t.loc[0].to_json()
-                send_data(test,t.loc[0]['news_source'])
-                print('Data sent')
-            else:
-                print('Skipped')
-        except:
-            test =t.loc[0].to_json()
-            send_data(test,t.loc[0]['news_source'])
-            
-    except Exception as e:
-        print(e)
+	    try:
+	        f  = open('urlparsed.txt','r')
+	        already_parsed = f.read().split('\n')
+	        f.close()
+	    except:
+	        f = open('urlparsed.txt','w')
+	        for i in url:
+	            try:
+	                i =i['href']
+	            except:
+	                pass
+	            f.write(str(i))
+	            f.write('\n')
+	        f.close()
+	       
+	    try:
+	        for i in already_parsed:
+	            try:
+	                url.remove(i)
+	            except:
+	                pass
+	        
+	        for i in url:
+	            already_parsed.append(i)
+	        
+	        f = open('urlparsed.txt','w')
+	        for i in already_parsed:
+	                try:
+	                    i =i['href']
+	                except:
+	                    pass
+	                f.write(str(i))
+	                f.write('\n')
+	        f.close()
+	    except:
+	        pass
+	        
+	    for i in url:    
+	        try :
+	            try:
+	                i =i['href']
+	            except:
+	                pass
+	            
+	            
+	            if 'https' not in i:
+	                if 'http' not in i:
+	                    i = j+i
+	            
+	            print('\n',i)
+	                
+	            response = requests.get(i,timeout=10)
+	            details = newspaper(i)
+	            count = len(details.article)
+	            publish_date = details.date_publish
+	            cr_date = details.date_download
+	            description = details.description
+	            summary = details.summary
+	            category = details.category
+	            if count > 1500:
+	                if len(description) > 10 or len(summary) > 10:
+	            
+	                    print("Appended")
+	                    asli.append(i)
+	                    
+	                else:
+	                    pass
+	            else:
+	                pass
+	        except:
+	            pass
+
+
+
+	print('fetching info')
+	headline=[]
+	timestamp=[]
+	AUTHORS =[]
+	SUMMARY=[]
+	date_crawled = []
+	news_source = [] 
+	full = []
+	img_url = []
+	for i in asli:
+	    chrome_options = Options()
+	    chrome_options.add_argument('--headless')
+	    chrome_options.add_argument('--no-sandbox')
+	    chrome_options.add_argument('--disable-dev-shm-usage')
+	    driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options = chrome_options)
+
+
+	    #driver = webdriver.Chrome('/home/priyanshu/project 10/chromedriver')
+	    driver.get(i)
+	    details = newspaper(i)
+	    headline.append(details.headline)
+	    timestamp.append(details.date_publish)
+	    author=''
+	    for i in details.authors:
+	        author = author + i
+	        author =author + ', '
+	        
+	    
+	    author= author[:-2]
+	    AUTHORS.append(author)
+	    
+	    if len(details.summary) > 10:
+	        SUMMARY.append(details.summary)
+	    else:
+	        SUMMARY.append(details.description)
+	    
+	    date_crawled.append(details.date_download)
+	    news_source.append(j)
+	    
+	    full.append(details.article)
+	    
+	    re = driver.find_elements_by_tag_name('img')
+	    for i in re:
+	        if'.jpg' in i.get_attribute('src'):
+	            im = i.get_attribute('src')
+	            break;
+	    if len(im)>3:
+	        img_url.append(im)
+	    else:
+	        img_url.append(None)
+	    print('Done inside')
+	    driver.close()
+
+	final = pd.DataFrame({'Title':headline,'Author':AUTHORS,'Summary':SUMMARY,
+	                          'full_text':full,'date_published':timestamp, 'date_crawled':date_crawled,
+	                          'news_source':news_source,'img':img_url})
+	    
+	for i in final.index:
+	    try:
+	        t = pd.DataFrame()
+	        t =t.append(final.loc[i])
+	        t.reset_index(drop=True, inplace=True)
+	        try:
+	            count = search(t.loc[0]['Title'],t.loc[0]['news_source'])
+	            print(count)
+	            if count < 25 :
+	                test =t.loc[0].to_json()
+	                send_data(test,t.loc[0]['news_source'])
+	                print('Data sent')
+	            else:
+	                print('Skipped')
+	        except:
+	            test =t.loc[0].to_json()
+	            send_data(test,t.loc[0]['news_source'])
+	            
+	    except Exception as e:
+	        print(e)
+	print('info fetched')
+
+
